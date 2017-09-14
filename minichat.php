83648e31a034f18e39b639a022aa8bcab81e4a62
farrah <marzini.f@gmail.com>
2017-09-14 13:56:04 +0200

    structure of the form realized

--- minichat.php
+++ minichat.php
@@ -7,8 +7,17 @@
   <!-- <script src="script.js"></script> -->
 </head>
 <body>
-  ...
-  <!-- Le reste du contenu -->
-  ...
+  <form class="" action="minichat_post.php" method="post">
+    <label for="pseudo">Votre pseudo<input type="text" name="pseudo" value=""></label><br>
+    <label for="message">Votre message <textarea name="message" rows="8" cols="80"></textarea></label><br>
+    <input type="submit" name="envoyer" value="envoyer">
+  </form>
+  <main>
+    <?php
+// mettre la requête pour se connecter à la base de données 
+// faire une requête pour afficher les 10 derniers articles
+// fermer la requete
+     ?>
+  </main>
 </body>
 </html>
