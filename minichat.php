4bb8271a5759c5411e6f44ba14790d39dcaa5a31
farrah <marzini.f@gmail.com>
2017-09-14 13:58:13 +0200

    started the code on the minichat.php

--- minichat.php
+++ minichat.php
@@ -14,9 +14,24 @@
   </form>
   <main>
     <?php
-// mettre la requête pour se connecter à la base de données 
-// faire une requête pour afficher les 10 derniers articles
-// fermer la requete
+
+// OK mettre la requête pour se connecter à la base de données
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+
+// faire une requête pour afficher les 10 derniers articles 
+
+// OK fermer la requete
+$marequete->closeCursor();
+
+
      ?>
   </main>
 </body>
